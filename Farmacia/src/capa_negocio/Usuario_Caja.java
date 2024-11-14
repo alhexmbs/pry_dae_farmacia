6aac44c15913e0fda75db5f17d3c843ed1680cc4
jsusurcia <jnthnur-gmail.com>
2024-11-14 10:38:58 -0500

    gaaa

--- Farmacia/src/capa_negocio/Usuario_Caja.java
+++ Farmacia/src/capa_negocio/Usuario_Caja.java
@@ -17,6 +17,17 @@ public class Usuario_Caja {
     ResultSet rs = null;
     public String loggedInUsername = "";
     
+    public ResultSet listarTrabajadores() throws Exception{
+        strSQL = "select * from usuario where id_rol != 1";
+        
+        try{
+            rs = objConectar.consultarBD(strSQL);
+            return rs;
+        }catch(Exception ex){
+            throw new Exception("Error al obtener los trabajadores --> " + ex.getMessage());
+        }
+    }
+    
     /*
         Cada vez que se asigne un nuevo usuario a una caja, se ejecuta lo siguiente...
         -El resto de atributos inicializan en nulos, exceptuuando el monto inicial que se pedirá antes de registrar la asignación
@@ -36,5 +47,28 @@ public class Usuario_Caja {
     /*
         Método para ir autoincrementando la cantidad de transacciones, filtrando si es el usuario
     */
+    public void actualizarCantTransacciones(Integer idUsuario, Integer idCaja) throws Exception{
+        strSQL = "UPDATE USUARIO_CAJA SET transacciones_realizadas = transacciones_realizadas + 1 "
+                + "WHERE id_usuario = " + idUsuario + " AND id_caja = " + idCaja;
+        
+        try{
+            objConectar.ejecutarBd(strSQL);
+        }catch(Exception ex){
+            throw new Exception("Error al actualizar el número de transacciones --> " + ex.getMessage());
+        }
+    }
     
+    /*
+        Método para verificar si existe una caja asignada a un usuario
+    */
+    public void verificarAsignacion(Integer idUsuario) throws Exception{
+        strSQL = "SELECT COUNT(*) AS asignacion_activa FROM USUARIO_CAJA "
+                + "WHERE id_usuario = " + idUsuario + " AND fecha_hora_apertura::date = CURRENT_DATE AND fecha_hora_cierre IS NULL";
+        
+        try{
+            objConectar.ejecutarBd(strSQL);
+        }catch(Exception ex){
+            throw new Exception("Error al verificar asignacion --> " + ex.getMessage());
+        }
+    }
 }
