13c87d176cbe53c18c5c5ec22a4ac1550fd7bece
jsusurcia <jnthnur-gmail.com>
2024-11-14 00:32:53 -0500

    tmre

--- Farmacia/src/capa_negocio/Usuario_Caja.java
+++ Farmacia/src/capa_negocio/Usuario_Caja.java
@@ -17,6 +17,11 @@ public class Usuario_Caja {
     ResultSet rs = null;
     public String loggedInUsername = "";
     
+    /*
+        Cada vez que se asigne un nuevo usuario a una caja, se ejecuta lo siguiente...
+        -El resto de atributos inicializan en nulos, exceptuuando el monto inicial que se pedirá antes de registrar la asignación
+        -La cantidad de transacciones empieza en 0, estas irán autoincrementando cada que se haga una venta (filtrando por usuario)
+    */
     public void asignarCajaUsuario(Integer idUsuario, Integer idCaja, float montoInicial) throws Exception{
         strSQL = "INSERT INTO USUARIO_CAJA (id_usuario, id_caja, monto_inicial, transacciones_realizadas) "
                 + "VALUES (" + idUsuario + ", " + idCaja + ", " + montoInicial + ", 0)";
@@ -28,5 +33,8 @@ public class Usuario_Caja {
         }
     }
     
+    /*
+        Método para ir autoincrementando la cantidad de transacciones, filtrando si es el usuario
+    */
     
 }
