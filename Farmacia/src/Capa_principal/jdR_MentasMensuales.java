b233e9a3a902736f0d41a46360b5b2f61d2b16ba
Katia <katiagil560@gmail.com>
2024-11-28 16:35:57 -0500

    Implementé la validación de formularios

--- Farmacia/src/Capa_principal/jdR_MentasMensuales.java
+++ Farmacia/src/Capa_principal/jdR_MentasMensuales.java
@@ -4,6 +4,8 @@ package Capa_principal;
 import capa_negocio.Reporte;
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.JOptionPane;
 import net.sf.jasperreports.swing.JRViewer;
 
@@ -15,26 +17,26 @@ public class jdR_MentasMensuales extends javax.swing.JDialog {
 
     public jdR_MentasMensuales(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
-        initComponents();
-                this.vistareporte.setVisible(false);
+        initComponents();                
+        this.vistareporte.setVisible(false);
 
+        
     }
 
+ 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
+        txtCategoria = new javax.swing.JTextField();
         vistareporte = new javax.swing.JDesktopPane();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        txaño = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-
-        jButton1.setText("jButton1");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
 
         javax.swing.GroupLayout vistareporteLayout = new javax.swing.GroupLayout(vistareporte);
         vistareporte.setLayout(vistareporteLayout);
@@ -44,49 +46,102 @@ public class jdR_MentasMensuales extends javax.swing.JDialog {
         );
         vistareporteLayout.setVerticalGroup(
             vistareporteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 473, Short.MAX_VALUE)
+            .addGap(0, 583, Short.MAX_VALUE)
+        );
+
+        jPanel1.setBackground(new java.awt.Color(246, 244, 235));
+
+        jLabel1.setFont(new java.awt.Font("sansserif", 1, 18)); // NOI18N
+        jLabel1.setText("Reporte de ventas mensuales");
+
+        jButton1.setBackground(new java.awt.Color(236, 177, 89));
+        jButton1.setText("Ver reporte");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Año:");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(78, 78, 78)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txaño, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 381, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(46, 46, 46))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(237, 237, 237))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(17, 17, 17)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jLabel2)
+                    .addComponent(txaño, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(19, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(vistareporte)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(20, 20, 20)
-                .addComponent(jButton1)
-                .addContainerGap(521, Short.MAX_VALUE))
+            .addComponent(vistareporte, javax.swing.GroupLayout.Alignment.TRAILING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(13, 13, 13)
-                .addComponent(jButton1)
-                .addGap(18, 18, 18)
-                .addComponent(vistareporte)
-                .addContainerGap())
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(vistareporte))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
- try {
+        try {
             Container contenedor = this.vistareporte;
             contenedor.setLayout(new BorderLayout());
 
-            JRViewer objReporte = new Reporte().reporteInterno("ventasMensuales.jasper", null);
+            Map parametros = new HashMap();
+
+            parametros.put("p_anio", Integer.parseInt(txaño.getText()));
+
+            JRViewer objReporte = new Reporte().reporteInterno("r_ventasMensuales.jasper", parametros);
             contenedor.add(objReporte);
             objReporte.setVisible(true);
 
             this.vistareporte.setVisible(true);
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(this, "Error reporte frm-->" +e.getMessage());
-        }    }//GEN-LAST:event_jButton1ActionPerformed
+            JOptionPane.showMessageDialog(this, "Error reporte frm-->" + e.getMessage());
+        }
 
- 
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+
+    
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField txaño;
+    private javax.swing.JTextField txtCategoria;
     private javax.swing.JDesktopPane vistareporte;
     // End of variables declaration//GEN-END:variables
 }
