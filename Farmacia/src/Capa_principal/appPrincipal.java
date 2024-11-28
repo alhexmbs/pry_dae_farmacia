64058a0d3fa53d08081921b6c49f5fa57ba4f628
artemis <artemisdevisa@gmail.com>
2024-11-28 18:26:43 -0500

    Subido

--- Farmacia/src/Capa_principal/appPrincipal.java
+++ Farmacia/src/Capa_principal/appPrincipal.java
@@ -3,8 +3,7 @@ package Capa_principal;
 public class appPrincipal {
 
     public static void main(String[] args) {
- frmFarmacia farmacia = new frmFarmacia("Administrador", 1, 1);
-        farmacia.setVisible(true);
+
         
 
         /*
