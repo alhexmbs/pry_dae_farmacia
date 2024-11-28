79bd180aab6ce0032e0df21065ccdd2c25a592c9
Katia <katiagil560@gmail.com>
2024-11-28 17:43:53 -0500

    reportesTerminados

--- Farmacia/src/capaDatos/datos.java
+++ Farmacia/src/capaDatos/datos.java
@@ -20,7 +20,7 @@ public class datos {
         this.driver = "org.postgresql.Driver";
         this.url = "jdbc:postgresql://25.59.118.186:5432/DAE_FARMACIA";
         //this.url = "jdbc:postgresql://localhost:5432/DAE_FARMACIA";
-        //this.user = "postgres";
+        this.user = "postgres";
         this.password = "visa2005";
         this.con = null;
     }
